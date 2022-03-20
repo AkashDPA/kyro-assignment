7dd43e2ccbd33a81877411e08fbfc4f2d3a43e53
Akash Pathade <akashpathadeakol@gmail.com>
2022-03-20 12:25:28 +0530

    cards, API complete

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,22 @@
-(function($){"use strict";var fullHeight=function(){$('.js-fullheight').css('height',$(window).height());$(window).resize(function(){$('.js-fullheight').css('height',$(window).height());});};fullHeight();$('#sidebarCollapse').on('click',function(){$('#sidebar').toggleClass('active');});})(jQuery);
+class Schedule_Class {
+    constructor(data = []) {
+        this.data = data
+        return this
+    }
+
+    getPersonNames() {
+        var personNames = new Set();
+        this.data.forEach(schedule => {
+            personNames.add(schedule.person_name);
+        })
+        return personNames;
+    }
+
+    getProjectNames() {
+        var projectNames = new Set();
+        this.data.forEach(schedule => {
+            projectNames.add(schedule.project_name);
+        })
+        return projectNames;
+    }
+}
